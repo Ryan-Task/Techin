60c8b61f80d05d73db175b113a5fd9d73e89fcfe
Ryan-Task <aryantotrinasrullah77@gmail.com>
2025-08-04 16:41:11 +0700

    coba aajh

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-use Illuminate\Support\Facades\Http;
-
-Route::get('/cek-github-{random}', function () {
-    $response = Http::get('https://api.github.com/');
-
-    if ($response->successful()) {
-        return response()->json([
-            'status' => 'ok',
-            'message' => 'GitHub dapat diakses.',
-            'headers' => $response->headers(),
-        ]);
-    } else {
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Tidak bisa mengakses GitHub.',
-            'code' => $response->status(),
-        ], $response->status());
-    }
-});
